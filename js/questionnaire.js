35061e2171de2273da651c8b705897967f460a70
Unknown <tbaroux@laposte.net>
2017-11-16 16:33:22 +0100

    Test ajlut quetion

--- js/questionnaire.js
+++ js/questionnaire.js
@@ -1,4 +1,29 @@
-function reponse() {
+var questions = [];
+
+function addQuestion() {
+	questions.push(document.getElementById('question').value);
+	document.getElementById('question').value = '';
+	showQuestionList();
+}
+
+function removeQuestion(position) {
+	questions.splice(position, 1);
+	showQuestionList();
+}
+
+function showQuestionList() {
+	var questionList = document.getElementById('questionList');
+	questionList.innerHTML = '';
+	for (var i = 0; i < question.length; i++) {
+		questionList.innerHTML += '<li onclick="removeQuestion('+i+');">'+question[i]+'</li>';
+	}
+}
+
+document.getElementById('ajoutQuestionBtn').addEventListener('click', addQuestion);
+
+
+
+/*function reponse() {
 	var resultat = document.getElementById("resultat");
 	
 	if ( document.getElementById('blanc').checked ) {
@@ -11,3 +36,4 @@ function reponse() {
 
 	}
 }
+*/
