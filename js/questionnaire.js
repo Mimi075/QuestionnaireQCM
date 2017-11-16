38c7861fa3218140cd4e6453d3f1b583e041c688
Unknown <tbaroux@laposte.net>
2017-11-16 14:59:31 +0100

    Remplacement switch par clause if

--- js/questionnaire.js
+++ js/questionnaire.js
@@ -1,20 +1,13 @@
 function reponse() {
-    var blanc = document.getElementById('blanc').checked;
-    var rouge = document.getElementById('rouge').checked;
-    var vert = document.getElementById('vert').checked;
-    var resultat = document.getElementById("resultat");
+	var resultat = document.getElementById("resultat");
+	
+	if ( document.getElementById('blanc').checked ) {
+		resultat.innerHTML = "Bonne réponse, vous avez répondu blanc";
+	} else if ( document.getElementById('rouge').checked ) {
+		resultat.innerHTML = "Mauvaise réponse, vous avez répondu rouge";
+	} else if ( document.getElementById('vert').checked ) {
+		resultat.innerHTML = "Mauvaise réponse, vous avez répondu vert";
+	} else {
 
-    switch (true) {
-    	case blanc:
-    		resultat.innerHTML = "Bonne réponse, vous avez répondu blanc";
-    		break;
-    	case rouge:
-    		resultat.innerHTML = "Mauvaise réponse, vous avez répondu rouge";
-    		break;
-    	case vert:
-    		resultat.innerHTML = "Mauvaise réponse, vous avez répondu vert";
-    		break;
-    	default:
-    		// statements_def
-    		break;
-    }
+	}
+}
