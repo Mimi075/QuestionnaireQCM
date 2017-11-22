68204e8e69b3d1025716362622cea85a99d19374
Mickael <mickael.camy@gmail.com>
2017-11-22 11:39:05 +0100

    suppression du 1er code

--- js/questionnaire.js
+++ js/questionnaire.js
@@ -23,17 +23,3 @@ document.getElementById('ajoutQuestionBtn').addEventListener('click', addQuestio
 
 
 
-/*function reponse() {
-	var resultat = document.getElementById("resultat");
-	
-	if ( document.getElementById('blanc').checked ) {
-		resultat.innerHTML = "Bonne réponse, vous avez répondu blanc";
-	} else if ( document.getElementById('rouge').checked ) {
-		resultat.innerHTML = "Mauvaise réponse, vous avez répondu rouge";
-	} else if ( document.getElementById('vert').checked ) {
-		resultat.innerHTML = "Mauvaise réponse, vous avez répondu vert";
-	} else {
-
-	}
-}
-*/
